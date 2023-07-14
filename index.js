dcf1430d2d532fac711c18214f6cab315fe08a24
Andrii Ratsilevych <andrii.racilewicz@gmail.com>
2023-07-14 10:46:06 +0200

    update READE.md

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ dotenv.config({ path: './.env'});
 
 const Db = process.env.ATLAS_URI;
 
-// await mongoose.connect(`${Db}`);
-// console.info('Connected to CookBook DataBase');
-
 const app = express();
 app.use(cors());
 const httpServer = http.createServer(app);
@@ -27,9 +24,6 @@ const httpServer = http.createServer(app);
   .catch((error) => {
     console.error(error);
   })
-
-// await mongoose.connect(Db);
-// console.info('Connected to CookBook DataBase');
 
 const server = new ApolloServer({
   typeDefs,
@@ -47,84 +41,3 @@ app.use(
 
 new Promise((resolve) => httpServer.listen({ port: process.env.PORT }, resolve));
 console.info(`🚀 Server ready at ${process.env.PORT}`);
-
-// mongoose.connect(Db, {useNewUrlParser: true})
-//   .then(() => {
-//     console.info('Connected to CookBook DataBase');
-//     return server.listen({ port: process.env.PORT });
-//   })
-//   .then((res) => {
-//     console.info(`🚀 Server ready at ${re.url}`);
-//   })
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// import {ApolloServer} from '@apollo/server';
-// import {startStandaloneServer} from '@apollo/server/standalone';
-// import {typeDefs} from './models/typeDefs.js';
-// import {resolvers} from './resolvers/resolvers.js';
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-// dotenv.config({ path: './.env'})
-// const Db = process.env.ATLAS_URI;
-// const PORT = process.env.PORT;
-
-/*const db = await mongoose.connect(`${Db}`);
-console.info(db ? 'Connected to CookBook DataBase' : 'Not connected toCookBook DataBase' );*/
-
-//  mongoose.connect(Db, {useNewUrlParser: true})
-//   .then(() => {
-//     console.info('Connected to CookBook DataBase');
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   })
-
-// const server = new ApolloServer({typeDefs, resolvers})
-// const { url } = await startStandaloneServer(server, {
-//   listen: {
-//       port: PORT,
-//   },
-// });
-// console.info(`🚀 Server ready at ${url}`);
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// import express from 'express';
-// import { ApolloServer } from 'apollo-server-express';
-// import {typeDefs} from './models/typeDefs.js';
-// import {resolvers} from './resolvers/resolvers.js';
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-// import cors from 'cors';
-// dotenv.config({ path: './.env'})
-// const Db = process.env.ATLAS_URI;
-// const PORT = process.env.PORT;
-
-// mongoose.connect(Db, {useNewUrlParser: true})
-//   .then(() => {
-//     console.info('Connected to CookBook DataBase');
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
-
-//   const server = new ApolloServer({
-//     typeDefs,
-//     resolvers,
-//   });
-
-//   const app = express();
-//   app.use(cors());
-
-//   server.start().then(() => {
-//     server.applyMiddleware({ app });
-//     app.listen({ port: PORT }, () => {
-//       console.info(`🚀 Server ready at ${PORT}`);
-//     });
-//   })
-
-  // app.get('/', (reg, res) => {
-  //   console.info('GraphQL Express server is ready');
-  // });
-  
